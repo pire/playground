9f8f581080a6b50fab0c9f8a40278606ad86ac52
pire <rob.mccormack@diascreative.com>
2014-02-26 10:14:35 +0000

    add the initial skrollr

---
+++ skrollr/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Skrollr play</title>
+</head>
+<body>
+
+    <div style="background: #efe; height: 400px"></div>
+    <div id="foo" data--50-bottom="background-color:rgb(0,0,255);" data-50-top="background-color:rgb(255,0,0);">WOOOT</div>
+
+    <script type="text/javascript" src="../libs/skrollr.min.js"></script>
+    <script type="text/javascript">
+    var s = skrollr.init();
+    </script>
+</body>
+</html>
